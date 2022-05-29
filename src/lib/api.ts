3c17d10a44ef8c1b99e42c3478dd8717cf620b01
Hiroroki <misosiru999@mail.com>
2022-05-29 15:55:29 +0900

    コンパイルエラ5

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,7 +10,7 @@ const serviceId: string = process.env.NEXT_PUBLIC_MICRO_CMS_SERVICE_ID ?? 'defau
 const baseUrl: string = `https://${serviceId}.microcms.io/api/v1`
 
 const apiKey: string = process.env.NEXT_PUBLIC_MICRO_CMS_API_KEY ?? 'default'
-const writeApiKey: string = "test"//process.env.NEXT_PUBLIC_MICRO_CMS_WRITE_API_KEY
+const writeApiKey: string = process.env.NEXT_PUBLIC_MICRO_CMS_WRITE_API_KEY ?? 'default'
 
 const params = (method: string, data?: {}) => {
     if (data) {
@@ -33,7 +33,7 @@ const params = (method: string, data?: {}) => {
 }
 
 // 記事を全件取得
-export const fetchAllPosts = async (): Promise<Post[]> => {
+export const fetchAllPosts = async (): Promise<Post[] | undefined> => {
     const data = await fetch(`${baseUrl}/blog`, params("GET"))
         .then(res => res.json())
         .catch(() => null)
@@ -44,7 +44,7 @@ export const fetchAllPosts = async (): Promise<Post[]> => {
 }
 
 // IDから個別の記事を取得
-export const fetchPostById = async (id: string): Promise<Post> => {
+export const fetchPostById = async (id: string): Promise<Post | undefined> => {
     const data = await fetch(`${baseUrl}/blog/${id}`, params("GET"))
         .then(res => res.json())
         .catch(() => null)
@@ -55,7 +55,7 @@ export const fetchPostById = async (id: string): Promise<Post> => {
 }
 
 // ページ番号によって記事を取得
-export const fetchPostsByPageNumber = async (pageNumber: number, limit: number): Promise<Post[]> => {
+export const fetchPostsByPageNumber = async (pageNumber: number, limit: number): Promise<Post[] | undefined> => {
     const data = await fetch(`${baseUrl}/blog?offset=${(pageNumber - 1) * 6}&limit=${limit}`, params("GET"))
         .then(res => res.json())
         .catch(() => null)
@@ -66,7 +66,7 @@ export const fetchPostsByPageNumber = async (pageNumber: number, limit: number):
 }
 
 // 最新の記事のみを取得
-export const fetchLatestPosts = async (limit: number): Promise<Post[]> => {
+export const fetchLatestPosts = async (limit: number): Promise<Post[] | undefined> => {
     const data = await fetch(`${baseUrl}/blog?limit=${limit}`, params("GET"))
         .then(res => res.json())
         .catch(() => null)
